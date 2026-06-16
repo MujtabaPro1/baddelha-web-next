9bc9e0c0b5ea27fbfbcef34252fb17293f3b576f
MujtabaPro1 <mujtabaszabist@gmail.com>
2026-06-16 15:22:38 +0400

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,32 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.baddelha.com.sa',
+          },
+        ],
+        destination: 'https://baddelha.com.sa/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'baddelha.com.sa',
+          },
+        ],
+        destination: 'https://baddelha.com.sa/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
